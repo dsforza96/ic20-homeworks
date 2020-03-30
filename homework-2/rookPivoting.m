d5b25516047c3ed84dd85fba3add5dc04fac73f2
Davide Sforza <sforza.1710262@studenti.uniroma1.it>
2020-03-30 19:13:42 +0200

    New rook pivoting

--- homework-2/rookPivoting.m
+++ homework-2/rookPivoting.m
@@ -1,9 +1,10 @@
 function [A, xperm] = rookPivoting(A, xperm, k)
 
-[M, R] = max(abs(A(k:end, k:end - 1)));
-s = find(all(M >= A(R, k:end - 1)'), 1);
-r = R(s) + k - 1;
-s = s + k - 1;
+[M1, R] = max(abs(A(k:end, k:end - 1)));
+[M2, S] = max(abs(A(k:end, k:end - 1)), [], 2);
+i = find(M1 == M2', 1);
+r = R(i) + k - 1;
+s = S(i) + k - 1;
 
 if r ~= k
      A([k, r], k:end) = A([r, k], k:end);
