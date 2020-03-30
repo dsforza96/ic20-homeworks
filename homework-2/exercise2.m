aa84b0c8676a934e170a9d61fdf563a8f23653cb
Davide Sforza <sforza.1710262@studenti.uniroma1.it>
2020-03-30 18:31:55 +0200

    Exercise 2 performance evaluation

--- homework-2/exercise2.m
+++ homework-2/exercise2.m
@@ -2,11 +2,39 @@ clear all
 close all
 clc
 
-n = 100;
-A = rand(n);
-b = rand(n, 1);
+sizes = [50, 100, 150, 200, 250];
+m = 20;  % Number of matrices on which to average results
 
-x = gaussianEliminationWithPivoting(A, b, @completePivoting);
+etimes = zeros(3, length(sizes));
 
-% y = A \ b;
-% mean(abs(x - y))
+for i=1:length(sizes)
+    for j=1:m
+        n = sizes(i);
+        A = rand(n);
+        b = rand(n, 1);
+
+        tic
+        gaussianEliminationWithPivoting(A, b, @naivePivoting);
+        etimes(1, i) = etimes(1, i) + toc;
+
+        tic
+        gaussianEliminationWithPivoting(A, b, @partialPivoting);
+        etimes(2, i) = etimes(2, i) + toc;
+
+        tic
+        gaussianEliminationWithPivoting(A, b, @completePivoting);
+        etimes(3, i) = etimes(3, i) + toc;
+    end
+end
+
+etimes = etimes ./ m;
+
+%%
+
+figure
+plot(sizes, etimes .* 1000)  % Converting to milliseconds
+
+xlabel 'Dimension (# rows)'
+ylabel 'Time (ms)'
+legend('Naive', 'Partial', 'Complete')
+
