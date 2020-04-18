3380b81438fe2d8ad5c46973d9e8fc5ca4f95e1f
Davide Sforza <sforza.1710262@studenti.uniroma1.it>
2020-04-18 23:09:24 +0200

    Update

--- homework-3/exercise-1/exercise1.m
+++ homework-3/exercise-1/exercise1.m
@@ -2,14 +2,15 @@ clear all
 close all
 clc
 
-eps = 0.01;
+eps = 0.001;
 
 A = rand(100);
-A = (A + A') ./ 2;
+A = (A + A') ./ 2; % Forcing the matrix to be symmetric
 
 [u1, lambda1] = epair(A, eps);
 
 [u2, lambda2] = deflation(A, u1, lambda1, eps);
 
-[v, l] = eigs(A, 3);
+[v, l] = eigs(A, 2);
 v = abs(v ./ max(abs(v)));
+u2 = abs(u2 ./ max(abs(u2)));
