9e567f397a7e8e2c5a0ece5db45a1438142f3bcb
Davide Sforza <sforza.1710262@studenti.uniroma1.it>
2020-04-19 18:18:06 +0200

    Refactoring

--- homework-3/exercise-2/exercise2.m
+++ homework-3/exercise-2/exercise2.m
@@ -30,40 +30,54 @@ L_inv = inv(L);
 x1 = epair(L_inv, eps);
 x2 = deflation(L_inv, x1, 0, eps);
 
-posneg_split = find(x2 < 0);
-mean_split = find(x2 < mean(x2));
-median_split = find(x2 < median(x2));
+posneg_split = x2 < 0;
+mean_split = x2 < mean(x2);
+median_split = x2 < median(x2);
 
-%%
+%% Showing results
 
 G = graph(A);
 
 blue = zeros(n, 3);
 blue(:, 3) = 1;
 
-subplot(2, 2, 1);
-cmap = blue;
-cmap(maxgould, :) = [1 0 0];
-plot(G, 'NodeColor', cmap, 'MarkerSize', 10);
-title Gould
+red_ind = [(1:n)' == maxgould, posneg_split, mean_split, median_split];
+titles = {'Gould', 'Pos-neg', 'Mean', 'Median'};
 
-subplot(2, 2, 2);
-cmap = blue;
-cmap(posneg_split, 1) = 1;
-cmap(posneg_split, 3) = 0;
-plot(G, 'NodeColor', cmap, 'MarkerSize', 10);
-title Pos-neg
+for i=1:4
+    subplot(2, 2, i);
 
-subplot(2, 2, 3);
-cmap = blue;
-cmap(mean_split, 1) = 1;
-cmap(mean_split, 3) = 0;
-plot(G, 'NodeColor', cmap, 'MarkerSize', 10);
-title Mean
+    cmap = blue;
+    cmap(red_ind(:, i), 1) = 1;
+    cmap(red_ind(:, i), 3) = 0;
 
-subplot(2, 2, 4);
-cmap = blue;
-cmap(median_split, 1) = 1;
-cmap(median_split, 3) = 0;
-plot(G, 'NodeColor', cmap, 'MarkerSize', 10);
-title Median
+    plot(G, 'NodeColor', cmap, 'MarkerSize', 7);
+    title(titles(i))
+end
+
+% subplot(2, 2, 1);
+% cmap = blue;
+% cmap(maxgould, :) = [1 0 0];
+% plot(G, 'NodeColor', cmap, 'MarkerSize', 10);
+% title Gould
+% 
+% subplot(2, 2, 2);
+% cmap = blue;
+% cmap(posneg_split, 1) = 1;
+% cmap(posneg_split, 3) = 0;
+% plot(G, 'NodeColor', cmap, 'MarkerSize', 10);
+% title Pos-neg
+% 
+% subplot(2, 2, 3);
+% cmap = blue;
+% cmap(mean_split, 1) = 1;
+% cmap(mean_split, 3) = 0;
+% plot(G, 'NodeColor', cmap, 'MarkerSize', 10);
+% title Mean
+% 
+% subplot(2, 2, 4);
+% cmap = blue;
+% cmap(median_split, 1) = 1;
+% cmap(median_split, 3) = 0;
+% plot(G, 'NodeColor', cmap, 'MarkerSize', 10);
+% title Median
