d71cecb755af2784ca58e903465123f34f7950aa
Davide Sforza <sforza.1710262@studenti.uniroma1.it>
2020-05-10 12:43:27 +0200

    Update

--- homework-4/exercise1.m
+++ homework-4/exercise1.m
@@ -6,8 +6,8 @@ n = 4;
 
 M = binary2RNS(0, n);
 
-X1 = randi(M);
-X2 = randi(M);
+X1 = randi(M - 1);
+X2 = randi(M - 1);
 
 [~, x1] = binary2RNS(X1, n);
 [~, x2] = binary2RNS(X2, n);
@@ -18,5 +18,6 @@ for i=1:3
     A = radix102RB(x1(i));
     B = radix102RB(x2(i));
 
-    S(i) = RB2radix10(RBSum(A, B));
+    s = RBSum(A, B);
+    S(i) = mod(RB2radix10(s), 2 .^ n - 2 + i);
 end
