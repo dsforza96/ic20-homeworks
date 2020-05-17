ae49144473fe49ad804e99d8c5c55f6fad0bd814
Davide Sforza <sforza.1710262@studenti.uniroma1.it>
2020-05-17 14:59:25 +0200

    Algoritmo di routing

--- homework-5/selfRoutingButterfly.m
+++ homework-5/selfRoutingButterfly.m
@@ -4,8 +4,40 @@ N = length(p);
 n = log2(N);
 
 P = zeros(N, 2 .* n);
+P(:, 1) = 1:N;
+
 S = ones(N ./ 2, n) .* 2;
+route = de2bi(p - 1, 'left-msb');
+nodes = de2bi(0:N ./ 2 - 1, 'left-msb');  %#ok<BDSCA>
 
+for i=1:n
+    perm = P(:, i .* 2 - 1);
+    setting = route(perm, i);
+    
+    even = setting(1:2:end);
+    odd = setting(2:2:end);
+    
+    S(:, i) = even;
+    
+    conflict = even == odd;
+    
+    if any(conflict)
+        S(conflict, i) = 3;
+        break
+    end
+    
+    cross = find(even) .* 2;
+    
+    perm([cross - 1; cross]) = perm([cross; cross - 1]);
+    P(:, i .* 2) = perm;
+    
+    if i < n 
+        source = [find(nodes(:, i)) .* 2 - 1; find(~nodes(:, i)) .* 2];
+        dest = [find(~nodes(:, i)) .* 2; find(nodes(:, i)) .* 2 - 1];
 
+        perm(source) = perm(dest);
+        P(:, i .* 2 + 1) = perm;
+    end
+end
 
 end
