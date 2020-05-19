262967512e946f825f15903c9658d9e31a658af2
Davide Sforza <sforza.1710262@studenti.uniroma1.it>
2020-05-19 11:30:21 +0200

    Update

--- homework-5/exercise2.m
+++ homework-5/exercise2.m
@@ -6,10 +6,18 @@ N = 100;
 n = 10;
 
 % Strictly non-blocking CLOS network
-m = 2 .* n - 1;
-r = ceil(N ./ n);
+m = 2 * n - 1;
+r = ceil(N / n);
+
+C_sn = (2 * r .* ceil(m / n) + m * ceil(r / n)) * n ^ 2; 
 
 % Rearrangeable CLOS network
 m = n;
 
+C_rn = (2 * r * ceil(m / n) + m * ceil(r / n)) * n ^ 2;
+
 % Comparing costs to the equivalent crossbar network
+C_cr = N ^ 2;
+
+G_cr_sn = C_cr / C_sn;
+G_cr_rn = C_cr / C_rn;
