5d130393b7fc20d974b97ca65f46e841a8b91a6f
Davide Sforza <sforza.1710262@studenti.uniroma1.it>
2020-05-19 11:57:21 +0200

    Completato es 2

--- homework-5/exercise2.m
+++ homework-5/exercise2.m
@@ -9,10 +9,20 @@ n = 10;
 m = 2 * n - 1;
 r = ceil(N / n);
 
+fprintf("Strictly non-blocking CLOS network:\n")
+fprintf("  First stage:   %d modules of size %d x %d\n", r, n, m)
+fprintf("  Second stage:  %d modules of size %d x %d\n", m, r, r)
+fprintf("  Third stage:   %d modules of size %d x %d\n", r, m, n)
+
 C_sn = (2 * r .* ceil(m / n) + m * ceil(r / n)) * n ^ 2; 
 
 % Rearrangeable CLOS network
 m = n;
+
+fprintf("\nRearrangeable CLOS network:\n")
+fprintf("  First stage:   %d modules of size %d x %d\n", r, n, m)
+fprintf("  Second stage:  %d modules of size %d x %d\n", m, r, r)
+fprintf("  Third stage:   %d modules of size %d x %d\n", r, m, n)
 
 C_rn = (2 * r * ceil(m / n) + m * ceil(r / n)) * n ^ 2;
 
@@ -21,3 +31,7 @@ C_cr = N ^ 2;
 
 G_cr_sn = C_cr / C_sn;
 G_cr_rn = C_cr / C_rn;
+
+fprintf("\nGain w.r.t. the %d x %d crossbar:\n", N, N)
+fprintf("  Strictly non-blocking CLOS:  %g\n", G_cr_sn)
+fprintf("  Rearrangeable CLOS:          %g\n", G_cr_rn)
